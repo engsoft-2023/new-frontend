28ad30e2c0e70a15f7012c398b163962974cd3a4
@julialsilva724 <julia.lsilva724@gmail.com>
2023-12-11 21:27:11 -0300

    Deleting DisplayMetrics.test.tsx
    Signed-off-by: Julia Leite <julia.lsilva724@gmail.com>
    Co-authored-by: Gabriel Henrique <gabrielhpr@usp.br>

--- components/DisplayMetrics/DisplayMetrics.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { DisplayMetrics } from "./DisplayMetrics"
-
-describe(DisplayMetrics, () => {
-  it ("Should render the Metrics")  
-})
